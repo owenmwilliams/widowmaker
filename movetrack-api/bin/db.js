660b9e30dea223d3da68d9481b7f2831580218ff
Owen Williams <owenm.williams@gmail.com>
2025-11-07 22:28:06 -0800

    Fix Cloud SQL Unix socket connection for Cloud Run

--- movetrack-api/bin/db.js
+++ movetrack-api/bin/db.js
@@ -5,7 +5,23 @@ const passwd = process.env.MT_DATALAYER_PASSWORD;
 const hostname = process.env.MT_DATALAYER_HOSTNAME;
 const port = process.env.MT_DATALAYER_PORT;
 const database = process.env.MT_DATALAYER_DATABASE;
+
+// Check if we're using Cloud SQL Unix socket (production)
+let connectionString;
+if (hostname && hostname.startsWith('/cloudsql/')) {
+    // Unix socket connection for Cloud Run
+    connectionString = {
+        host: hostname,
+        port: port,
+        database: database,
+        user: user,
+        password: passwd
+    };
+} else {
+    // TCP connection for local development
+    connectionString = `postgres://${user}:${passwd}@${hostname}:${port}/${database}`;
+}
     
-const db = pgp(`postgres://${user}:${passwd}@${hostname}:${port}/${database}`);
+const db = pgp(connectionString);
 
 exports.db = db;
